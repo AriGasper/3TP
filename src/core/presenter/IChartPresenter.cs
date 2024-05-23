957e899102813448a847913c9110256089c0f931
AriGasper <arishagura@gmail.com>
2024-05-24 00:42:20 +0300

    Добавьте файлы проекта.

---
+++ src/core/presenter/IChartPresenter.cs
@@ -0,0 +1,26 @@
+﻿using TP3.src.view;
+using System.Collections.Generic;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace TP3.core
+{
+    public interface IChartPresenter
+    {
+        IChartView View { set; }
+
+        /// <summary>
+        /// Выводит на экран график [series]
+        /// </summary>
+        void ShowChart(List<Series> series);
+        /// <summary>
+        /// Выводит на экран сообщение [info]
+        /// </summary>
+        void ShowAdditionalInfo(string info);
+        /// <summary>
+        /// Выводит на экран таблицу.
+        /// </summary>
+        /// <param name="columnsName">Названия столбцов</param>
+        /// <param name="rows">Данные таблицы</param>
+        void ShowGrid(List<string> columnsName, List<List<string>> rows);
+    }
+}
